b06c17ed7d914bd061b421ab1fb6388ba8e8b419
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-08-14 01:08:52 +0300

    Firebase Authentication ve Sahte Auth Service Metotlarının Yazılması

--- lib/services/fake_auth_service.dart
+++ lib/services/fake_auth_service.dart
@@ -2,18 +2,21 @@ import 'package:flutter_lovers/model/user_model.dart';
 import 'package:flutter_lovers/services/auth_base.dart';
 
 class FakeAuthenticationService implements AuthBase {
+  String userID = "123123123123123213123123123";
+
   @override
-  Future<User> currentUser() {
-    return null;
+  Future<User> currentUser() async {
+    return await Future.value(User(userID: userID));
   }
 
   @override
   Future<bool> signOut() {
-    return null;
+    return Future.value(true);
   }
 
   @override
-  Future<User> singInAnonymously() {
-    return null;
+  Future<User> singInAnonymously() async {
+    return await Future.delayed(
+        Duration(seconds: 2), () => User(userID: userID));
   }
 }
--- lib/services/firebase_auth_service.dart
+++ lib/services/firebase_auth_service.dart
@@ -1,19 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_lovers/model/user_model.dart';
 import 'package:flutter_lovers/services/auth_base.dart';
 
 class FirebaseAuthService implements AuthBase {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
   @override
-  Future<User> currentUser() {
-    return null;
+  Future<User> currentUser() async {
+    try {
+      FirebaseUser user = await _firebaseAuth.currentUser();
+      return _userFromFirebase(user);
+    } catch (e) {
+      print("HATA CURRENT USER" + e.toString());
+    }
+  }
+
+  User _userFromFirebase(FirebaseUser user) {
+    if (user == null) return null;
+    return User(userID: user.uid);
   }
 
   @override
-  Future<bool> signOut() {
-    return null;
+  Future<bool> signOut() async {
+    try {
+      await _firebaseAuth.signOut();
+      return true;
+    } catch (e) {
+      print("sign out hata:" + e.toString());
+      return false;
+    }
   }
 
   @override
-  Future<User> singInAnonymously() {
-    return null;
+  Future<User> singInAnonymously() async {
+    try {
+      AuthResult sonuc = await _firebaseAuth.signInAnonymously();
+      return _userFromFirebase(sonuc.user);
+    } catch (e) {
+      print("anonim giris hata:" + e.toString());
+      return null;
+    }
   }
 }
