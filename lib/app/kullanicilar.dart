64abf646134e199c8298e539e46aac1183b8933c
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-09-25 18:56:42 +0300

    firestore pagination bölüm 2

--- lib/app/kullanicilar.dart
+++ lib/app/kullanicilar.dart
@@ -21,7 +21,18 @@ class _KullanicilarPageState extends State<KullanicilarPage> {
   void initState() {
     // TODO: implement initState
     super.initState();
+
     getUser(_enSonGetirilenUser);
+    _scrollController.addListener(() {
+      if (_scrollController.position.atEdge) {
+        if (_scrollController.position == 0) {
+          print("En tepedeyiz");
+        } else {
+          print("listenin sonundayız");
+          getUser(_enSonGetirilenUser);
+        }
+      }
+    });
   }
 
   @override
@@ -29,23 +40,58 @@ class _KullanicilarPageState extends State<KullanicilarPage> {
     UserModel _userModel = Provider.of<UserModel>(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Kullanicilar"),
-      ),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: Text("Kullanicilar"),
+          actions: <Widget>[
+            FlatButton(
+                onPressed: () async {
+                  await getUser(_enSonGetirilenUser);
+                },
+                child: Text("Next Users"))
+          ],
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: _tumKullanicilar.length == 0
+                    ? Center(
+                        child: Text("Kullanıcı yok"),
+                      )
+                    : _kullaniciListesiniOlustur()),
+            _isLoading
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Container(),
+          ],
+        ));
   }
 
   getUser(User enSonGetirilenUser) async {
+    if (!_hasMore) {
+      print(
+          "Getirilecek eleman kalmadı o yüzden firestore rahatsız edilmeyecek");
+      return;
+    }
+    if (_isLoading) {
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
     QuerySnapshot _querySnapshot;
 
     if (enSonGetirilenUser == null) {
+      print("İlk defa kullanıcılar getiriliyor");
       _querySnapshot = await Firestore.instance
           .collection("users")
           .orderBy("userName")
           .limit(_getirilecekElemanSayisi)
           .getDocuments();
     } else {
+      print("Sonraki kullanıcılar getiriliyor");
       _querySnapshot = await Firestore.instance
           .collection("users")
           .orderBy("userName")
@@ -54,11 +100,33 @@ class _KullanicilarPageState extends State<KullanicilarPage> {
           .getDocuments();
     }
 
+    if (_querySnapshot.documents.length < _getirilecekElemanSayisi) {
+      _hasMore = false;
+    }
+
     for (DocumentSnapshot snap in _querySnapshot.documents) {
       User _tekUser = User.fromMap(snap.data);
       _tumKullanicilar.add(_tekUser);
+      print("Getirilen user name:" + _tekUser.userName);
     }
 
-    enSonGetirilenUser = _tumKullanicilar.last;
+    _enSonGetirilenUser = _tumKullanicilar.last;
+    print("en son getirilen user name:" + _enSonGetirilenUser.userName);
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  _kullaniciListesiniOlustur() {
+    return ListView.builder(
+      controller: _scrollController,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(_tumKullanicilar[index].userName),
+        );
+      },
+      itemCount: _tumKullanicilar.length,
+    );
   }
 }
