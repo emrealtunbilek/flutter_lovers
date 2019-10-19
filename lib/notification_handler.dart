663c0cdfb2ac8fa23ddc4338c07ce9bd75a11276
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-10-19 03:25:07 +0300

    local notification plugini ile bildirimleri göstermek

--- lib/notification_handler.dart
+++ lib/notification_handler.dart
@@ -1,12 +1,16 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_lovers/common_widget/platform_duyarli_alert_dialog.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 Future<void> myBackgroundMessageHandler(Map<String, dynamic> message) {
   if (message.containsKey('data')) {
     // Handle data message
     final dynamic data = message['data'];
     print("Arka planda gelen data:" + message["data"].toString());
+    NotificationHandler.showNotification(message);
   }
 
   return Future<void>.value();
@@ -22,16 +26,24 @@ class NotificationHandler {
   NotificationHandler._internal();
 
   initializeFCMNotification(BuildContext context) async {
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+    var initializationSettingsIOS = IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    var initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+
     _fcm.subscribeToTopic("spor");
+
+    String token = await _fcm.getToken();
+    print("token :" + token);
 
     _fcm.configure(
       onMessage: (Map<String, dynamic> message) async {
         print("onMessage tetiklendi: $message");
-        PlatformDuyarliAlertDialog(
-                baslik: "test baslik",
-                icerik: "Test Icerik",
-                anaButonYazisi: "OK")
-            .goster(context);
+        showNotification(message);
       },
       onBackgroundMessage: myBackgroundMessageHandler,
       onLaunch: (Map<String, dynamic> message) async {
@@ -42,4 +54,25 @@ class NotificationHandler {
       },
     );
   }
+
+  static void showNotification(Map<String, dynamic> message) async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        '1234', 'Yeni Mesaj', 'your channel description',
+        importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(0, message["data"]["title"],
+        message["data"]["message"], platformChannelSpecifics,
+        payload: 'Bildirim tıklanılınca aktarılan değer');
+  }
+
+  Future onSelectNotification(String payload) {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+  }
+
+  Future onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) {}
 }
